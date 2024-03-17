203e70c78d58c46c7b3991a2c6446a014c754e8a
DeproGecko <163779060+DeproGecko@users.noreply.github.com>
2024-03-17 23:41:43 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Watching Queue","Made by DeproGecko"];
 
 
 let currentIndex = 0;
